53852e80f16f2821a09edecc500a04ecd964194d
Léa Rostoker <lea.rostoker.12@gmail.com>
2021-04-29 15:01:41 +0200

    //RandomUFO

--- probability_space_invaders/Assets/Scripts/SpaceshipUfo.cs
+++ probability_space_invaders/Assets/Scripts/SpaceshipUfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,91 @@ public class SpaceshipUfo : MonoBehaviour
 {
     public float speed = 3f;
     // Start is called before the first frame update
+    private PlayerController playerController;
+
+    void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
     void Start()
     {
-        speed = Random.Range(3f, 6f);
+        //liste des probabilités
+        double[] proba = new double[8];
+
+        // proba[0] = p(speed = 3f)
+        // proba[7] = p(speed = 10f)
+
+        //calcul du paramètre p
+        double param = 0.8; // changer en fonction du score du joueur
+        int score = playerController.Score;
+        if (score <= 2500)
+        {
+            param = 0.8;
+        }
+        if (score > 2500 && score <= 6000)
+        {
+            param = 0.5;
+        }
+        else
+        {
+            param = 0.2;
+        }
+        // calcul des probabilités 
+        for (int i = 0; i < 8; i++)
+        {
+            proba[i] = param * System.Math.Pow((1 - param), i + 3 - 1); // p(x=k) = p(1-p)^(k-1)
+        }
+        // proba[7] = 1 - (proba[0] + proba[1] + proba[2] + proba[3] + proba[4] + proba[5] + proba[6] + proba[7] + proba[8]);
+
+        // random 
+        double randomNumber = (double)UnityEngine.Random.Range(0f, 1f);
+
+        if (randomNumber >= 0 && randomNumber < proba[7])
+        {
+            print("speed=10f");
+            speed = 10f;
+        }
+        if (randomNumber >= proba[7] && randomNumber < proba[6])
+        {
+            print("speed=9f");
+            speed = 9f;
+        }
+        if (randomNumber >= proba[6] && randomNumber < proba[5])
+        {
+            print("speed=8f");
+            speed = 8f;
+        }
+        if (randomNumber >= proba[5] && randomNumber < proba[4])
+        {
+            print("speed=7f");
+            speed = 7f;
+        }
+        if (randomNumber >= proba[4] && randomNumber < proba[3])
+        {
+            print("speed=6f");
+            speed = 6f;
+        }
+        if (randomNumber >= proba[3] && randomNumber < proba[2])
+        {
+            print("speed=5f");
+            speed = 5f;
+        }
+        if (randomNumber >= proba[2] && randomNumber < proba[1])
+        {
+            print("speed=4f");
+            speed = 4f;
+        }
+        if (randomNumber >= proba[1] && randomNumber < proba[0])
+        {
+            print("speed=3f");
+            speed = 3f;
+        }
+        else
+        {
+            print("speed=else");
+            speed = 6.5f;
+        }
     }
 
     // Update is called once per frame
@@ -17,8 +100,10 @@ public class SpaceshipUfo : MonoBehaviour
         transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
 
-    private void OnTriggerStay2D(Collider2D collision){
-        if(collision.CompareTag("Destroy")){
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Destroy"))
+        {
             Destroy(gameObject);
         }
     }
--- probability_space_invaders/Assets/Scripts/SpawnUfo.cs
+++ probability_space_invaders/Assets/Scripts/SpawnUfo.cs
@@ -6,25 +6,133 @@ public class SpawnUfo : MonoBehaviour
 {
     public GameObject ufo;
     //Aleatoire
-    public float firstSpawn = 1f, nextSpawn = 3f;
+    public float firstSpawn = 1f, nextSpawn = 25f;
+    private PlayerController playerController;
+
+    void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        //liste des probabilités
+        double[] proba = new double[11];
+        // proba[0] = p(nextSpawn = 20f)
+        // proba[10] = p(nextSpawn = 30f)
+
+        //calcul du paramètre p
+        int score = playerController.Score;
+        double lambda = 1;
+        if (score <= 2500)
+        {
+            lambda = 1;
+        }
+        if (score > 2500 && score <= 6000)
+        {
+            lambda = 4;
+        }
+        else
+        {
+            lambda = 10;
+        }
+
+        // calcul des probabilités 
+        for (int i = 0; i < 8; i++)
+        {
+            double fact = i;
+
+            for (double j = fact - 1; j >= 1; j--)
+            {
+                fact = fact * j;
+                print(fact);
+            }
+
+            proba[i] = (System.Math.Pow(lambda, i) / fact) * System.Math.Exp(-1 * lambda); //formule poisson
+        }
+        // proba[7] = 1 - (proba[0] + proba[1] + proba[2] + proba[3] + proba[4] + proba[5] + proba[6] + proba[7] + proba[8]);
+
+        // random 
+        double randomNumber = (double)UnityEngine.Random.Range(0f, 1f);
+        //randomNumber += 20;
+        //randomNumber *= 30;
+
+        if (randomNumber >= 0 && randomNumber < proba[0])
+        {
+            print("nextSpawn = 20");
+            nextSpawn = 20f;
+        }
+        if (randomNumber >= proba[0] && randomNumber < proba[1])
+        {
+            print("nextSpawn = 21");
+            nextSpawn = 21f;
+        }
+        if (randomNumber >= proba[1] && randomNumber < proba[2])
+        {
+            print("nextSpawn = 22");
+            nextSpawn = 22f;
+        }
+        if (randomNumber >= proba[2] && randomNumber < proba[3])
+        {
+            print("nextSpawn = 23");
+            nextSpawn = 23f;
+        }
+        if (randomNumber >= proba[3] && randomNumber < proba[4])
+        {
+            print("nextSpawn = 24");
+            nextSpawn = 24f;
+        }
+        if (randomNumber >= proba[4] && randomNumber < proba[5])
+        {
+            print("nextSpawn = 25");
+            nextSpawn = 25f;
+        }
+        if (randomNumber >= proba[5] && randomNumber < proba[6])
+        {
+            print("nextSpawn = 26");
+            nextSpawn = 26f;
+        }
+        if (randomNumber >= proba[6] && randomNumber < proba[7])
+        {
+            print("nextSpawn = 27");
+            nextSpawn = 27f;
+        }
+        if (randomNumber >= proba[7] && randomNumber < proba[8])
+        {
+            print("nextSpawn = 28");
+            nextSpawn = 28f;
+        }
+        if (randomNumber >= proba[8] && randomNumber < proba[9])
+        {
+            print("nextSpawn = 29");
+            nextSpawn = 29f;
+        }
+        if (randomNumber >= proba[9] && randomNumber < proba[10])
+        {
+            print("nextSpawn = 30");
+            nextSpawn = 30f;
+        }
+        
+
         firstSpawn = Random.Range(15f, 20f);
-        nextSpawn = Random.Range(20f, 30f);
+        //nextSpawn = Random.Range(20f, 30f); // loi de poisson
         InvokeRepeating("spawnUfoPrefab", firstSpawn, nextSpawn);
     }
 
-    void spawnUfoPrefab(){
-        if(!GameObject.Find("UFO")){
+    void spawnUfoPrefab()
+    {
+        if (!GameObject.Find("UFO"))
+        {
             GameObject go;
             go = Instantiate(ufo, transform.position, Quaternion.identity);
             go.name = "UFO";
         }
     }
 
-    public void ufoStopSpawn(){
+    public void ufoStopSpawn()
+    {
         CancelInvoke();
     }
+
 }
